872c193dd99b1b95bc788e91f3dfd5bddaf2f798
Maya Nair <msnair@emory.edu>
2017-12-02 19:43:44 -0500

    Update sc_api_requests.py

--- sc_api_requests.py
+++ sc_api_requests.py
@@ -6,10 +6,6 @@ def songs(source):
     client = soundcloud.Client(client_id='Bo3dsyRIYdM3bp3SnNQ9xl3ax0IyWu38')
 
     # find all tracks with the genre chosen by user
-    try:
-        tracks = client.get('/tracks', genres=source)
-    except Exception, e:
-        print 'Error: %s, Status Code: %d' % (e.message, e.response.status_code)
+    tracks = client.get('/tracks', genres=source)
 
-    text = tracks.json()['songs']
-    return text
+    return tracks
